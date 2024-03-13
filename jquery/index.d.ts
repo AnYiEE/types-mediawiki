54571653136873e0598e30e3861533c1333e76eb
安忆 <i@anyi.in>
2024-03-13 22:06:19 +0800

    feat: add more jQuery plugins check other authors/commits at https://github.com/wikimedia-gadgets/types-mediawiki/pull/34

--- jquery/index.d.ts
+++ jquery/index.d.ts
@@ -1,4 +1,13 @@
 import './client';
 import './collapsibleTabs';
 import './colorUtil';
+import './confirmable';
+import './footHovzer';
+import './highlightText';
+import './htmlform';
+import './lengthLimit';
+import './makeCollapsible';
+import './spinner';
+import './tablesorter';
 import './textSelection';
+import './updateTooltipAccessKeys';
